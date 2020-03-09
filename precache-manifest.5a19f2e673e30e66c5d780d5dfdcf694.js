d307e2c2c7fe1dfde6b66f619d29ba546643d7d4
Moses <lazaromoses77@gmail.com>
2020-03-09 17:37:45 +0300

    Updates

---
+++ precache-manifest.5a19f2e673e30e66c5d780d5dfdcf694.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14a28abb52cc7c6759066c2706a980f1",
+    "url": "/Weather-App2/index.html"
+  },
+  {
+    "revision": "40eb05806e5fa9abecdf",
+    "url": "/Weather-App2/static/css/main.071839ba.chunk.css"
+  },
+  {
+    "revision": "189d2234024951b819a8",
+    "url": "/Weather-App2/static/js/2.1acba19f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Weather-App2/static/js/2.1acba19f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40eb05806e5fa9abecdf",
+    "url": "/Weather-App2/static/js/main.55982e38.chunk.js"
+  },
+  {
+    "revision": "425e3701bc5ccd7fdd08",
+    "url": "/Weather-App2/static/js/runtime-main.4e4e4ef4.js"
+  }
+]);
